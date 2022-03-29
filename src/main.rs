eec4890d42e12612264caac5720652f6d15811ea
Phil Jeffes <phil@electrocatstudios.com>
2022-03-29 17:09:11 +0200

    Show screen and run message pump

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+// SDL2 Library Imports
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+use std::time::Duration;
+
+fn main() -> Result<(), String> {
+    println!("Starting Rusteroids");
+
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let _window = video_subsystem.window("Rusteroids", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        // Handle events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => {
+                    break 'running;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
+                },
+                _ => {} 
+            }
+        }
+
+        // Time management
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
 }
